21ed7ecc5ef058ead68006a00c9ca4765a0d3f3c
saurusss <40891607+saurusss@users.noreply.github.com>
2018-07-18 23:49:15 +0900

    업데이트 테스트

--- more_lambda.py
+++ more_lambda.py
@@ -2,3 +2,8 @@ points = [ { 'x' : 2, 'y' : 3 },
 { 'x' : 4, 'y' : 1 } ]
 points.sort(key=lambda i : i['y'])
 print(points)
+
+# dataframe 연습
+import pandas as pd
+pdf = pd.DataFrame(points)
+print(pdf)
