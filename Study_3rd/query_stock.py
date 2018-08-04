50050c905a23e7716ba8f9b2e94a3b7f1fbb72f0
saurusss <40891607+saurusss@users.noreply.github.com>
2018-08-05 00:30:36 +0900

    180805@home

--- Study_3rd/query_stock.py
+++ Study_3rd/query_stock.py
@@ -1,124 +1,70 @@
-#pandas-datareaer 오류 수정
-import datetime
-import pandas as pd
-pd.core.common.is_list_like = pd.api.types.is_list_like
-##
-#from pandas_datareader import data  
-import pandas_datareader as pdr
-import fix_yahoo_finance as yf
-yf.pdr_override()
-import matplotlib.pyplot as plt
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import pandas_datareader.data as web
+import fix_yahoo_finance as yf
+yf.pdr_override()
+import pandas as pd
+pd.core.common.is_list_like = pd.api.types.is_list_like
 
-def query_gs(code):
-    start_date = '1996-05-06' #startdate를 1996년으로 설정해두면 가장 오래된 데이터부터 전부 가져올 수 있다.
-    # tickers = '067160.KQ' #  아프리카tv ticker(종목코드)
-    # afreeca = pdr.data.get_data_yahoo(tickers, start_date)
-    # print(afreeca)
-    #tickers = '035420.KS' #  네이버의 ticker(종목코드)
-    tickers = '.'.join(code[:2])
-    gs = pdr.data.get_data_yahoo(tickers, code[2])
-    return gs
+from pandas import Series, DataFrame
 
-def print_menu():
-    print()
-    menu1 = input("종목코드(6자리): ")
-    menu2 = input("KS or KQ) : ")
-    menu3 = input("start date : ")
-    menu = [menu1, menu2, menu3]
-    return menu
-
-
-
-
-
-#Average 산출
-def ma(gss,calcT):
-    for i in calcT:
-        maN =  gss['Adj Close'].rolling(window=i).mean()
-        maC = 'MA' + str(i)
-        gs.insert(len(gs.columns), maC, maN)
-        #print(gs)
-
-
-
-
-# ma5 = ma(gs, 5)
-# ma20 =ma(gs, 20)
-# ma60 = ma(gs,60)
-# ma120 = ma(gs,120)
-
-# 주가 이동평균 계산
-# ma5 = gs['Adj Close'].rolling(window=5).mean()
-# gs.insert(len(gs.columns), "MA5", ma5)
-
-# ma20 = gs['Adj Close'].rolling(window=20).mean()
-# gs.insert(len(gs.columns), "MA20", ma20)
-
-# ma60 = gs['Adj Close'].rolling(window=60).mean()
-# gs.insert(len(gs.columns), "MA60", ma60)
-# ma120 = gs['Adj Close'].rolling(window=120).mean()
-# gs.insert(len(gs.columns), "MA120", ma120)
-
-plt.plot(gs.index, gs['Adj Close'], label="Adj Close")
-
-
-def make_graph(calcT):
-    for i in calcT:
-        plt.plot(gs.index, gs['MA'+str(i)], label='MA'+str(i))
-
-make_graph(calcT)
-# plt.plot(gs.index, gs['MA5'], label="MA5")
-# plt.plot(gs.index, gs['MA20'], label="MA20")
-# plt.plot(gs.index, gs['MA60'], label="MA60")
-# plt.plot(gs.index, gs['MA120'], label="MA120")
-plt.legend(loc='best')
-plt.grid()
-plt.show()
-
-# def run():
-#         while 1:
-#         tickers = print_menu()
-#         print(tickers)
-
-# if __name__ == "__main__":
-#     run()
-
-menu = print_menu()
-gs = query_gs(menu)
-# calcT = [5, 20, 60, 120]
-calcT = [60, 120]
-ma(gs, calcT)
-
-
-class MyWindow(QMainWindow):
+class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setupUI()
 
     def setupUI(self):
-        self.setGeometry(800, 400, 300, 150)
+        self.setGeometry(600, 200, 1200, 600)
+        self.setWindowTitle("PyChart Viewer v0.1")
+        self.setWindowIcon(QIcon('icon.png'))
 
-        # Label
-        label = QLabel("종목코드", self)
-        label.move(20, 20)
+        self.lineEdit = QLineEdit()
+        self.pushButton = QPushButton("차트그리기")
+        self.pushButton.clicked.connect(self.pushButtonClicked)
 
-        # LineEdit
-        self.lineEdit = QLineEdit("", self)
-        self.lineEdit.move(80, 20)
-        # self.lineEdit.textChanged.connect(self.lineEditChanged) # character 입력시 statusbar update
-        self.lineEdit.returnPressed.connect(self.lineEditChanged) # return 입력시 statusbat update
+        self.fig = plt.Figure()
+        self.canvas = FigureCanvas(self.fig)
 
-        # StatusBar
-        self.statusBar = QStatusBar(self)
-        self.setStatusBar(self.statusBar)
+        leftLayout = QVBoxLayout()
+        leftLayout.addWidget(self.canvas)
 
-    def lineEditChanged(self):
-        self.statusBar.showMessage(self.lineEdit.text())
+        # Right Layout
+        rightLayout = QVBoxLayout()
+        rightLayout.addWidget(self.lineEdit)
+        rightLayout.addWidget(self.pushButton)
+        rightLayout.addStretch(1)
+
+        layout = QHBoxLayout()
+        layout.addLayout(leftLayout)
+        layout.addLayout(rightLayout)
+        layout.setStretchFactor(leftLayout, 1)
+        layout.setStretchFactor(rightLayout, 0)
+
+        self.setLayout(layout)
+
+    def pushButtonClicked(self):
+        code = self.lineEdit.text()
+        #df = web.DataReader(code, "yahoo")
+        ticker = code + '.KS'
+       
+        df = web.get_data_yahoo(ticker, '2017-01-01')
+        df['MA20'] = df['Adj Close'].rolling(window=20).mean()
+        df['MA60'] = df['Adj Close'].rolling(window=60).mean()
+
+        ax = self.fig.add_subplot(111)
+        ax.plot(df.index, df['Adj Close'], label='Adj Close')
+        ax.plot(df.index, df['MA20'], label='MA20')
+        ax.plot(df.index, df['MA60'], label='MA60')
+        ax.legend(loc='upper right')
+        ax.grid()
+
+        self.canvas.draw()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    mywindow = MyWindow()
-    mywindow.show()
+    window = MyWindow()
+    window.show()
     app.exec_()
