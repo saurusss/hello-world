0de5988d14ea10452b85630a6172975009c0d08a
saurusss <40891607+saurusss@users.noreply.github.com>
2018-08-19 04:43:24 +0900

    update@home

---
+++ location/calDistance.py
@@ -0,0 +1,16 @@
+import sqlite3
+import pandas as pd
+import numpy as numpy
+# loc = pd.read_excel(r'C:\Users\sauru\Documents\GitHub\hello-world\location\loc_180818.xlsx')
+# print(loc)
+# 
+con = sqlite3.connect(r'C:\Users\sauru\Documents\GitHub\hello-world\location\location.db')
+cursor = con.cursor()
+
+PDB = cursor.execute("SELECT * FROM position 
+                WHERE  shortBran IS NULL )
+# for i in loc:
+#     cursor.execute("INSERT INTO position VALUES (loc.mgmtNum[i], loc.buildName[i], loc.addr[i], loc.latitude[i], loc.longitude[i])")
+ 
+con.commit()
+con.close()
---
+++ location/locationPd.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as numpy
+import matplotlib.pyplot as plt
+
+loc = pd.read_excel(r'C:\Users\sauru\Documents\GitHub\hello-world\location\loc_180818.xlsx')
+print(loc)
