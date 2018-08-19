f707ca1afb25b60fc10b3834a6bc66ead4c3b9f8
saurusss <40891607+saurusss@users.noreply.github.com>
2018-08-19 21:17:36 +0900

    Signed-off-by: saurusss <40891607+saurusss@users.noreply.github.com>

--- location/makeCols.py
+++ location/makeCols.py
@@ -4,7 +4,7 @@ import math
 loc = pd.read_excel(r'C:\Users\sauru\Documents\GitHub\hello-world\location\loc_180818.xlsx')
 print(loc)
 
-addDf = pd.DataFrema()
+addDf = pd.DataFrame()
 
 for i in range(5):
     print(loc.mgmtNum[i], loc.latitude[i], loc.longitude[i])
@@ -13,9 +13,5 @@ for i in range(5):
         distY = abs(loc.latitude[i] - loc.latitude[j])
         distX = abs(loc.longitude[i]-loc.longitude[j])
         dist = math.sqrt(distX**2 + distY**2)
-        loc.loc[j].'D'+ mgmtNum[i] = dist
-
-        print(add_col)
-   
-    # print(add_col)
-    # loc[loc.mgmtNum[i]] = add_col
+        print(loc.loc[j])
+        # loc.loc[j].'D'+ mgmtNum[i] = dist
