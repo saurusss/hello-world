c986f2cfaa3fe8ced0028a6aee620502c6be70f0
saurusss <40891607+saurusss@users.noreply.github.com>
2018-08-19 12:29:09 +0900

    update@home

--- location/locationDbAltTbl.py
+++ location/locationDbAltTbl.py
@@ -4,6 +4,7 @@ import numpy as numpy
 
 con = sqlite3.connect(r'C:\Users\sauru\Documents\GitHub\hello-world\location\location.db')
 cursor = con.cursor()
+#최기 branch 검색결과 nearestBranch
 cursor.execute("ALTER TABLE position Add nearestBranch TEXT NULL")
  
 con.commit()
