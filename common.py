25db439105541cbcf34b56404107aa2f2a3a1786
unknown <saurusss62@gmail.com>
2018-08-21 10:33:34 +0900

    push@office

--- common.py
+++
@@ -1,7 +0,0 @@
-import pandas_datareader.data as web
-import fix_yahoo_finance as yf
-yf.pdr_override()
-import pandas as pd
-pd.core.common.is_list_like = pd.api.types.is_list_like
-
-df = web.get_data_yahoo('055550.KS', '2017-01-01')
