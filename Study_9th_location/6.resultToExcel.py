e6f66f0807277a0a893ea646af8a8a5040fb8d2c
saurusss <40891607+saurusss@users.noreply.github.com>
2018-08-25 22:39:08 +0900

    update by Git Gui@home

--- Study_9th_location/6.resultToExcel.py
+++ Study_9th_location/6.resultToExcel.py
@@ -8,4 +8,4 @@ conn = sqlite3.connect(r'C:\Temp\loc_post.db')
 df = pd.read_sql_query("select * from position ", conn)
 
 df.to_csv('c:\\TEMP\\nrstBR.csv', mode='w')
-print("*"*10, "end of job", "*"*10)
+print("*"*10, " end of job ", "*"*10)
