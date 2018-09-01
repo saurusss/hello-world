fbe554c4240def053b36f87e63f1f6185cab697e
saurusss <40891607+saurusss@users.noreply.github.com>
2018-09-01 16:48:38 +0900

    update @home

--- Study_9th_Calulation_distance/calcDistance.py
+++ Study_9th_Calulation_distance/calcDistance.py
@@ -10,13 +10,13 @@ R = 6373.0
 # lon1 = radians( 127.5473771)
 # lat2 = radians( 37.8310677	) # 가평우체국
 # lon2 = radians( 127.5107432)
-# # 엑셀 계산 6,540
+# # 엑셀 계산 6,540  # 파이썬 계산 6,531
 
 lat1 = radians( 37.6858862	) # 고양가좌동(취)
 lon1 = radians( 126.7194845)
 lat2 = radians( 37.6829634	) # 고양대화동우체국
 lon2 = radians( 126.7555092 )
-# 엑셀 계산 3,219
+# 엑셀 계산 3,219   파이썬 계산 3.187736
 
 dlon = lon2 - lon1 
 dlat = lat2 - lat1
@@ -26,4 +26,3 @@ c = 2 * atan2(sqrt(a), sqrt(1 - a))
 distance = R * c 
 print("Result:", distance) 
 print("Should be:", 278.546, "km")
-# 파이썬 계산 6,531
