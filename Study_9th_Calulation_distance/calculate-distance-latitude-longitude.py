84d7c3c11df7bda5da5b33c263414b95519a8aea
saurusss <40891607+saurusss@users.noreply.github.com>
2018-09-01 17:46:47 +0900

    update@home radian + 거리계산

---
+++ Study_9th_Calulation_distance/calculate-distance-latitude-longitude.py
@@ -0,0 +1,16 @@
+ #!/usr/bin/env python 
+ # http://www.platoscave.net/blog/2009/oct/5/calculate-distance-latitude-longitude-python/ 
+ # Haversine formula example in Python # Author: Wayne Dyck 
+ import math 
+ def distance(origin, destination): 
+    lat1, lon1 = origin 
+    lat2, lon2 = destination 
+    radius = 6371 # Km
+    dlat = math.radians(lat2-lat1) 
+    dlon = math.radians(lon2-lon1) 
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+       * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2) 
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a)) 
+    d = radius * c 
+    
+    return d
