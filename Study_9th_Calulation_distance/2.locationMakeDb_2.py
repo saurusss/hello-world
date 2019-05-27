5b26b7454fe9a9975f6aad45d76b0f74fd529390
sauruss <saurusss@gmail.com>
2019-05-27 18:54:50 +0900

    5/27 study

--- Study_9th_Calulation_distance/2.locationMakeDb_2.py
+++ Study_9th_Calulation_distance/2.locationMakeDb_2.py
@@ -1,15 +1,16 @@
 import sqlite3
 import pandas as pd
-import numpy as numpy
+# import numpy as numpy
 import os
 
 
-dbPath = os.getenv('homedrive') + os.getenv('homepath') + r"\OneDrive\Documents\KOC\180807_우체국\작업관리정보\주소"
-# dbpath = os.getenv('homedrive') + os.getenv('homepath') + r"\Documents\Github\hello-world\Study_9th_Calulation_distance"
+# dbPath = os.getenv('homedrive') + os.getenv('homepath') + r"\OneDrive\Documents\KOC\180807_우체국\작업관리정보\주소"
+dbPath = os.getenv('homedrive') + os.getenv('homepath') + r"\Documents\Github\hello-world\Study_9th_Calulation_distance"
 
 # conn = sqlite3.connect(dbPath + r'\loc_post.db')
 
-excelPath = os.getenv('homedrive') + os.getenv('homepath') + r"\OneDrive\Documents\KOC\180807_우체국\작업관리정보\주소"
+# excelPath = os.getenv('homedrive') + os.getenv('homepath') + r"\OneDrive\Documents\KOC\180807_우체국\작업관리정보\주소"
+excelPath = os.getenv('homedrive') + os.getenv('homepath') + r"\Documents\Github\hello-world\Study_9th_Calulation_distance"
 
 loc = pd.read_excel(excelPath + r'\nrstBR_180921.xlsx')
 # loc = pd.read_excel(r'C:\temp\loc_180826.xlsx')
---
+++ Study_9th_Calulation_distance/test.py
@@ -0,0 +1,3 @@
+import os
+excelPath = os.getenv('homedrive') + os.getenv('homepath') + r"\Documents\Github\hello-world\Study_9th_Calulation_distance"
+print(excelPath)
